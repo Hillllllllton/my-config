515e2dc954be41b5679f89155970fcdca3687ae5
Hillllllllton <honlung1012@gmail.com>
2023-08-21 12:23:05 +0800

    init

---
+++ nvim/lua/user/nvim-tree.lua
@@ -0,0 +1,24 @@
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+
+nvim_tree.setup {
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  disable_netrw = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+}
+
+require"nvim-web-devicons".setup {
+  color_icons = true;
+  default = true;
+  strict = true;
+}
+require'nvim-web-devicons'.get_icons()
---
+++ nvim/lua/user/treesitter.lua
@@ -0,0 +1,39 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = { "lua", "vim", "json", "javascript", "python" },
+  sync_install = false,
+  ignore_install = {""},   --list of language which want to ignore_install
+
+  auto_install = false,   --recommended false 
+
+  highlight = {
+    enable = true,
+    disable = {""},
+  },
+
+  autopair = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+    disable = {"python", "css"},
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  }
+}
+ 
